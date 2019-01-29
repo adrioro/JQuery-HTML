42dc841730c8113eb322a91dd753f8895d759216
adrioro <artistsvillage@me.com>
2019-01-29 16:08:37 -0500

    Fifth commit

--- index.js
+++ index.js
@@ -5,5 +5,14 @@ $("#pOneId").click(function () {
 $("#button2").click(function () {
     $(".paragraph2").html("This is the new paragraph.");
     alert($(".paragraph2").html());
+});
 
+$("#button3").click(function () {
+    var element = $(".paragraph3");
+    element.html("I changed the paragraph and its CSS style.");
+    element.css("color", "blue");
+});
+$(".paragraph4").click(function () {
+    $(this).css("background-color", "rgb(156, 242, 240)");
+    $(this).css("border", "3px solid rgb(32, 11, 93)");
 });
